aaf3c3e8092259958c6d92c9e787e81d37f70831
Ayden Kirke <aydenkirke@Aydens-MacBook-Pro.local>
2020-03-06 13:47:09 -0500

    Labour and material work

---
+++ index.js
@@ -0,0 +1,9 @@
+// SPOILERS
+
+// While you're here, if you want to know how many headings exist in a document:
+console.log(`There are ${document.querySelectorAll('h2').length} <h2> elements!`)
+
+// If you wanted a list of them and their distance from the top of the document:
+document.querySelectorAll('h2').forEach(heading => {
+  console.log(`Heading: "${heading.textContent}" is ${heading.offsetTop}px from the top of the document`)
+})
