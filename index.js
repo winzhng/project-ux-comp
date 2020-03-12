958209c76ed3c5c5f5483a9462c020e95c8fdc35
Ayden Kirke <aydenkirke@Aydens-MacBook-Pro.local>
2020-03-12 13:34:28 -0400

    commit

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 // While you're here, if you want to know how many headings exist in a document:
 console.log(`There are ${document.querySelectorAll('h2').length} <h2> elements!`)
 
+let $sq = document.querySelector('.square');
+
+$sq.addEventListener('click', (event) => {
+  $sq.classList.toggle('spin')
+});
+
 // If you wanted a list of them and their distance from the top of the document:
 document.querySelectorAll('h2').forEach(heading => {
   console.log(`Heading: "${heading.textContent}" is ${heading.offsetTop}px from the top of the document`)
@@ -19,3 +25,4 @@ $(document).ready(function(){
       $("#myCarousel").carousel('next');
   });
 });
+
