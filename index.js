430d13cfd0d59ac78d44d0f4dc373972600e5598
Your Name <email@example.com>
2020-04-01 19:38:29 -0400

    accord done, scrollspy WIP

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ document.querySelectorAll('h2').forEach(heading => {
   console.log(`Heading: "${heading.textContent}" is ${heading.offsetTop}px from the top of the document`)
 })
 
-let haveHitBottom = false 
-
 let theStateOfTheInterface = (event) => {
     
     let winH = document.documentElement.clientHeight
     let winW = document.documentElement.clientWidth
     let docH = document.documentElement.scrollHeight
     let docW = document.documentElement.scrollWidth
-    let winY = window.scrollY 
+    let winY = window.scrollY
     let winX = window.scrollX 
     let maxY = docH - winH 
     let maxX = docW - winW
@@ -22,13 +20,15 @@ let theStateOfTheInterface = (event) => {
 
 document.querySelector('.panel').innerHTML = `
 <ul class="panel" id="panel_nav">
-<li><a class="panel_item" href="#enviro"><strong>Environmental Benefits</strong></a></li>
-<li><a class="panel_item" href="#labour"><strong>Labour and Materials</strong></a></li>
-<li><a class="panel_faq" href="#materials">Cost Effective Materials</a></li>
-<li><a class="panel_faq" href="#lab_int">Less Labour Intensive</a></li>
-<li><a class="panel_faq" href="#install">Installation Process</a></li>
-<li><a class="panel_item" href="#flex"><strong>Flexibility</strong></a></li>
-<li><a class="panel_item" href="#cost"><strong>Affordability</strong></a></li>
+  <li><a class="panel_item" href="#design"><strong>About</strong></a></li>
+  <li><a class="panel_item" href="#enviro"><strong>Environmental Benefits</strong></a></li>
+  <li><a class="panel_item" href="#labour"><strong>Labour and Materials</strong></a></li>
+  <li><a class="panel_faq" href="#materials">Cost Effective Materials</a></li>
+  <li><a class="panel_faq" href="#lab_int">Less Labour Intensive</a></li>
+  <li><a class="panel_faq" href="#install">Installation Process</a></li>
+  <li><a class="panel_item" href="#flex"><strong>Flexibility</strong></a></li>
+  <li><a class="panel_item" href="#cost"><strong>Affordability</strong></a></li>
+</ul>
 `
 }
 window.addEventListener('scroll', theStateOfTheInterface)
